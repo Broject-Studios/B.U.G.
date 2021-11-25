e1e9f0844cdce62823c023f5c6e7b514afb94325
niek schoone <niek.schoone@quicknet.nl>
2021-11-25 19:15:04 +0100

    some changes to obstacle spawner stages. Not finished yet

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
+    [SerializeField] private GameObject background;
 
     [SerializeField] private List<GameObject> obstaclesStageOne = new List<GameObject>();
 
@@ -17,9 +18,21 @@ public class ObstacleSpawner : MonoBehaviour
 
     [SerializeField] private float spawnDelay;
 
+    private bool stageOne = false;
+
     private void OnEnable()
     {
         StartCoroutine("DropObstaclesStageOne");
+        stageOne = true;
+    }
+
+
+    private void Update()
+    {
+        if (background.transform.position.y > stageOneOffY && stageOne)
+        {
+            StartCoroutine("DropObstaclesStageTwo");
+        }
     }
 
 
@@ -28,6 +41,16 @@ public class ObstacleSpawner : MonoBehaviour
         while(true)
         {
             GameObject.Instantiate(obstaclesStageOne[Random.Range(0, obstaclesStageOne.Count)], new Vector3(Random.Range(min, max), transform.position.y, 0), Quaternion.identity);
+
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+
+    IEnumerator DropObstaclesStageTwo()
+    {
+        while(true)
+        {
+            GameObject.Instantiate(obstaclesStageTwo[Random.Range(0, obstaclesStageTwo.Count)], new Vector3(Random.Range(min, max), transform.position.y, 0), Quaternion.identity);
 
             yield return new WaitForSeconds(spawnDelay);
         }
